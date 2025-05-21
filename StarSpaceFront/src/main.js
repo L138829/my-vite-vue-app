e913e1272077fe867058ad6f3b3039f7ed12d74c
L138829 <l138829129@gmail.com>
2025-05-21 22:46:05 +0800

    Update main.js

--- StarSpaceFront/src/main.js
+++ StarSpaceFront/src/main.js
@@ -2,7 +2,13 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router/index.js';
+// ——— 全局引入 Element Plus ———
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+
 
 createApp(App)
   .use(router) // 挂载路由器实例
+  .use(ElementPlus) // 挂载 Element Plus
   .mount('#app')
